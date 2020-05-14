2a4df8015a56ec64d1f6670e779f05298fdeb3e0
Andrew <andrew@Andrews-MacBook-Pro.local>
2020-05-14 16:13:46 +0100

    fixed getplants query

--- web/app/routes/getPlants.js
+++ web/app/routes/getPlants.js
@@ -3,9 +3,8 @@ const { query } = require('../helpers/db')
 module.exports = getPlants = async (req, res) => {
     let result
     try {
-        result = await query(`INSERT INTO gardens (name) VALUES ("test")`)
+        result = await query(`SELECT * FROM plants`)
     } catch (error) {
-        console.log(error)
         res.json({ error: "something went wrong" })
     }
     res.json(result)
