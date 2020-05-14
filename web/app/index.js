649b08558676c811b96ea003ca7868723804e03c
Andrew <andrew@Andrews-MacBook-Pro.local>
2020-05-14 20:44:35 +0100

    added register api

--- web/app/index.js
+++ web/app/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const { initialiseDB } = require('./helpers/db');
 
 const app = express();
+app.use(express.json());
+
 const port = 3000;
 const dbConfig = {
     host: 'db',
@@ -21,9 +23,12 @@ app.get("/", (req, res) => res.sendFile(path.join(__dirname, '/views/index.html'
 
 // import our routes
 const getPlants = require('./routes/getPlants');
+const register = require('./routes/register');
 
 // define our routes
 app.get('/api/getPlants', getPlants);
+app.post('/api/register', register);
+
 
 app.listen(port, () => {
     console.log(`web is running on container port ${port}`);
---
+++ web/app/routes/register.js
@@ -0,0 +1,11 @@
+const { query } = require('../helpers/db')
+
+module.exports = getPlants = async ({ body }, res) => {
+    let result
+    try {
+        result = await query(`INSERT INTO users (name, email, password) VALUES ('${body.name}', '${body.email}', '${body.password}')`)
+    } catch (error) {
+        res.json({ error: "something went wrong" })
+    }
+    res.json({ status: "okay" })
+}
